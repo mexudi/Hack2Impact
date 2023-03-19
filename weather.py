70e138c7f2043c0b0316e9b345783fbd845ce664
mexudi <soufianmexudi20@gmail.com>
2023-03-19 05:47:03 +0000

    using weather api

---
+++ weather.py
@@ -0,0 +1,25 @@
+import requests
+import math
+
+def get_weather_condition(api_key, location,country_code):
+    # URL for OpenWeatherMap API
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={location},{country_code}&appid={api_key}"
+    
+    
+    # Make a GET request to the API
+    response = requests.get(url)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Extract the weather condition from the API response
+        weather_data = response.json()
+        weather_condition = weather_data["weather"][0]["description"]
+        main = weather_data['main']
+        temp = math.floor(weather_data['main']['temp'] - 273.15)
+        max_temp = math.floor(weather_data['main']['temp_max'] - 273.15)
+        min_temp = math.floor(weather_data['main']['temp_min'] - 273.15)
+        humidity = weather_data['main']['humidity']
+        return f"The weather condition in {location} is {weather_condition}. The current temperture is {temp}, and the humidity is: {humidity}"
+    else:
+        # If the request was unsuccessful, return an error message
+        return "Error: Unable to retrieve weather data."
